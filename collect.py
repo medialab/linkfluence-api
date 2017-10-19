29f5dd163a274750e47b3b12d8db8d8db0ea622d
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2017-10-19 17:31:57 +0200

    simple linkfluence API downloader

---
+++ collect.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import os, json
+import hashlib
+import requests
+
+from config import API_URL, PROJECT_ID, AUTH_TOKEN
+
+if not os.path.exists(".cache"):
+    os.makedirs(".cache")
+
+def download(route="", args={}):
+    url = "%s%s%s.json" % (API_URL, PROJECT_ID, route)
+    hashurl = hashlib.md5('%s/%s' % (url, json.dumps(args))).hexdigest()
+    cache = os.path.join(".cache", hashurl)
+    if os.path.exists(cache):
+        with open(cache) as f:
+            return json.load(f)
+    headers = {
+      "Authorization": "Bearer %s" % AUTH_TOKEN,
+      "Content-Type": "application/json"
+    }
+    if not args:
+        res = requests.get(url, headers=headers)
+    else:
+        res = requests.post(url, headers=headers, data=json.dumps(args))
+    data = res.json()
+    with open(cache, "w") as f:
+        json.dump(data, f)
+    return data
+
+# Get project settings
+settings = download()
+
+# Get clusters
+clusters = download("/stories", {
+  "interval": "weeks",
+  "from": "2016-05-01T00:00:00+01:00",
+  "to": "2017-07-01T00:00:00+02:00",
+  "tz": "Europe/Paris"
+})
+
+# Get top keywords
+keywords = download("/insights/cloud", {
+  "fields": [],
+  "metrics": ["doc", "reach", "impression"],
+  "from": "2016-05-01T00:00:00+01:00",
+  "to": "2017-07-01T00:00:00+02:00",
+  "tz": "Europe/Paris"
+})
