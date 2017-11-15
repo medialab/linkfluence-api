f77cba54a0062388c06ad5396f6d1aafac0d98b1
samaingw <samain.gwenael@laposte.net>
2017-11-15 11:18:04 +0100

    Top links retrieving

--- collect.py
+++ collect.py
@@ -48,19 +48,54 @@ def download(route="", args={}):
 #settings = download()
 
 # Get top links
-def collect_top_links(frm, to, startIndex, rsltCount, rt=False):
-	return download("/insights/most-shared-url", {
+def collect_top_links_period(frm, to, focus=None, rt=False):
+    args = {
           "interval": "day",
           "from": frm,#"2016-05-01T00:00:00+01:00",
           "to": to,#"2017-07-01T00:00:00+02:00",
           "tz": "Europe/Paris",
-          "limit": rsltCount,#2000,
-          "start":startIndex,#i*2000
           #"sortBy":"volumetry",
           "flag":{"rt":rt}
-        })
+        }
+    if focus:
+        args["focuses"] = [{"id":focus,"include":True}]
+        args["fctx"] = [focus]
+    return download("/insights/most-shared-url", args)['mostSharedUrl']
 
+def collect_top_links(startdate, enddate, focus=None, days=1, rt=False):
+    results = []
+    dat = startdate
+    dt = dat.isoformat() + "+02:00"
+    end = enddate
+    while dat < end:
+        print dat.isoformat()[:10], focus
+        enddat = dat + timedelta(days=days)
+        enddt = enddat.isoformat() + "+02:00"
+        res = collect_top_links_period(dt, enddt, focus, rt)
+        for val in res:
+            val["date"] = dat.isoformat()[:10]
+            results.append(val)
+        dat = enddat
+        dt = dat.isoformat() + "+02:00"
+    return results
 
+def store_top_links(startdate, enddate, days=1, focus=("", None), rt=False):
+    top_links = collect_top_links(startdate, enddate, focus[1], days, rt)
+    suffix = ""
+    if focus[1]:
+        suffix = "_" + focus[0]
+    if days == 1:
+        suffix += "_daily"
+    elif days == 7:
+        suffix += "_weekly"
+
+#    headers = ["date", "normalized", "reversed"]
+ #       with open(os.path.join("data", key + suffix + ".csv"), "w") as f:
+  #          print >> f, ",".join(headers)
+   #         for row in words[key]:
+    #            print >> f, ",".join([format_for_csv(row[h]) for h in headers])
+    with open(os.path.join("data", "toplinks" + suffix + ".json"), "w") as f:
+         json.dump(top_links, f)
 # Get clusters
 def collect_stories_period_page(frm, to, startIndex, rsltCount, rt=False):
     return download("/stories", {
@@ -78,8 +113,8 @@ def store_stories(startdate, enddate, rt=False):
     sd = startdate.isoformat() + "+02:00"
     ed = enddate.isoformat() + "+02:00"
     suffix = '' if rt == False else '-withRT'
-    for j in range(50):
-        i = j+97
+    for i in range(100):
+#        i = j+99
         rslt_size = 1000
         with open('stories-'+str(i*rslt_size)+'-'+str((i+1)*rslt_size-1)+suffix+'.json', 'w') as f:
     	    print i*rslt_size
@@ -144,12 +179,13 @@ def store_words(startdate, enddate, days=1, focus=("", None)):
 
 if __name__ == "__main__":
     store_stories(datetime(2016, 5, 1), datetime(2017, 7, 1), False)
-#    for focus in [
-#      ("hamon", 83192),
-#      ("macron", 82709),
-#      ("fillon", 82719),
-#      ("melenchon", 82713),
-#      ("le-pen", 103736)
-#    ]:
-#        store_words(datetime(2017, 3, 3), datetime(2017, 7, 1), 1, focus)
-#        store_words(datetime(2016, 5, 1), datetime(2017, 7, 1), 7, focus)
+    for focus in [
+      ("hamon", 83192),
+      ("macron", 82709),
+      ("fillon", 82719),
+      ("melenchon", 82713),
+      ("le-pen", 103736)
+    ]:
+        store_words(datetime(2017, 3, 3), datetime(2017, 7, 1), 1, focus)
+        store_words(datetime(2016, 5, 1), datetime(2017, 7, 1), 7, focus)
+        store_top_links(datetime(2017, 5, 1), datetime(2017, 7, 1), 1, focus)
